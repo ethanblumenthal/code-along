22333a84b6f93219f25ab17d9f7cd596cbaf2d7a
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-04-03 20:01:52 -0500

    created redux form for creating streams

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import App from './components/App'
 import reducers from './reducers'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const store = createStore(
   reducers,
   composeEnhancers(applyMiddleware())
