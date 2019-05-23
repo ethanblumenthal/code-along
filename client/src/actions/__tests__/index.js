4b5881175dd22d1cad6fa00dcabada607d42f912
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-05-23 14:19:57 -0500

    setup enzyme for testing and created tests for reducers

---
+++ client/src/actions/__tests__/index.js
@@ -0,0 +1,14 @@
+import { createStream } from '../index'
+import { CREATE_STREAM } from '../types'
+
+describe('createStream', () => {
+  it('has the correct type', () => {
+    const action = createStream()
+    expect(action.type).toEqual(CREATE_STREAM)
+  })
+
+  it('has the correct payload', () => {
+    const action = createStream({ title: 'My Stream', description: 'This is a test', googleId: '123456789' })
+    expect(action.payload).toEqual({ title: 'My Stream', description: 'This is a test', googleId: '123456789' })
+  })
+})
